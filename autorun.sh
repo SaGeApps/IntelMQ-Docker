b49e8cf06cb9a0ae35fa63d25ade179814d369c8
Sooraj Antony <soorajantony@live.in>
2018-11-08 14:43:13 +0530

    cmd update

---
+++ autorun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/SaGeApps/IntelMQ-Docker.git
+docker build -t "sage:intelmq" .
+docker run -p 8081:80 -d sage:intelmq
+firefox "127.0.0.1:8081"
+#sudo apt install nfs-kernel-server
+#echo "$(pwd)	*(rw,sync,no_subtree_check)"
