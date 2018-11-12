ec749ffcf8eb9097b4ecb9b278cd806e154aa597
Sooraj Antony <soorajantony@live.in>
2018-11-12 19:12:12 +0530

    patch #2: server manual start check added

--- autorun.sh
+++ autorun.sh
@@ -3,6 +3,7 @@ git clone https://github.com/SaGeApps/IntelMQ-Docker.git
 cd "IntelMQ-Docker"
 docker build -t "sage:intelmq" .
 docker run -p 8081:80 -d sage:intelmq
+docker exec -ti $(docker ps -q) sh -c "service apache2 start"
 firefox "127.0.0.1:8081"
 #sudo apt install nfs-kernel-server
 #echo "$(pwd)	*(rw,sync,no_subtree_check)"
